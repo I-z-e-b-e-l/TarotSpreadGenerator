1505a2c03f4d4a71357330c9d8143c01a232af2e
Izebel <izebel@rocketmail.com>
2022-01-25 21:39:01 -0500

    Add files via upload

---
+++ JavaScript-Tarot-Generator/scripts.js
@@ -0,0 +1,139 @@
+arcana = ["The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor", "The Hierophant",
+"The Lovers",
+"The Chariot",
+"Strength",
+"The Hermit",
+"Wheel of Fortune",
+"Justice",
+"The Hanged Man",
+"Death",
+"Temperance",
+"The Devil",
+"The Tower",
+"The Star",
+"The Moon",
+"The Sun",
+"Judgement",
+"The World",
+"Ace of Cups",
+"Two of Cups",
+"Three of Cups",
+"Four of Cups",
+"Five of Cups",
+"Six of Cups",
+"Seven of Cups",
+"Eight of Cups",
+"Nine of Cups",
+"Ten of Cups",
+"Page of Cups",
+"Knight of Cups",
+"Queen of Cups",
+"King of Cups",
+"Ace of Swords",
+"Two of Swords",
+"Three of Swords",
+"Four of Swords",
+"Five of Swords",
+"Six of Swords",
+"Seven of Swords",
+"Eight of Swords",
+"Nine of Swords",
+"Ten of Swords",
+"Page of Swords",
+"Knight of Swords",
+"Queen of Swords",
+"King of Swords",
+"Ace of Rods",
+"Two of Rods",
+"Three of Rods",
+"Four of Rods",
+"Five of Rods",
+"Six of Rods",
+"Seven of Rods",
+"Eight of Rods",
+"Nine of Rods",
+"Ten of Rods",
+"Page of Rods",
+"Knight of Rods",
+"Queen of Rods",
+"King of Rods",
+"Ace of Pentacles",
+"Two of Pentacles",
+"Three of Pentacles",
+"Four of Pentacles",
+"Five of Pentacles",
+"Six of Pentacles",
+"Seven of Pentacles",
+"Eight of Pentacles",
+"Nine of Pentacles",
+"Ten of Pentacles",
+"Page of Pentacles",
+"Knight of Pentacles",
+"Queen of Pentacles",
+"King of Pentacles",
+]
+
+spread = []
+
+//take in length of spread from user
+
+let numberOfCards = parseInt(prompt("How many cards would you like in the spread?"), 10)
+console.log(`This is the number of cards in the spread: ${numberOfCards}`)
+
+//create a random nmber that is less or equal to the total number of card in the arcana array
+
+
+
+//add cards to the spread by first indexing an item in the array and saving it as card
+
+
+function createSpread() {
+    while (spread.length <= numberOfCards){
+        //define a new random value each iteration
+        let min = Math.ceil(0);
+        let max = Math.floor(arcana.length);
+        let randomValue = Math.floor(Math.random() * (max - min + 1) + min)
+
+        // console.log(`This is the random value: ${randomValue}`)
+        card = arcana[randomValue]
+        //generate a random binary value
+        let reversed = Math.floor(Math.random() * 2)
+        console.log(`reversed = ${reversed}`)
+
+
+        //write an if statement to attach "reversed" to the value of card
+        if (reversed === 1){
+            let card = card + " Reversed"
+            console.log(card)
+            spread.splice(0,0,card)
+
+        } else if (reversed === 0) {
+            console.log(card)
+            //add card to spread or you get an infinite loop
+            spread.splice(0,0,card)
+        }
+
+
+        
+    }
+
+
+}
+
+createSpread()
+
+console.log(spread)
+
+
+  
+
+// parseInt(text, 10);
+
+// card = random.choice(arcana)
+// arcana.pop(arcana.index(card))
+
+//array.pop
+
+//splice() method adds new items to an array
+//The first parameter (2) defines the position where new elements should be added (spliced in).
+//The second parameter (0) defines how many elements should be removed.
